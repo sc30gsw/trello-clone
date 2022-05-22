9975a327aea217acd86122950cc639df8be6edb3
sc30gsw <haidoushi875@gmail.com>
2022-05-23 08:02:46 +0900

    タスクカードボタンのスタイリング

--- src/components/task/TaskCards.tsx
+++ src/components/task/TaskCards.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { TaskCard } from "../task/TaskCard";
 import { AddTaskCardButton } from "./button/AddTaskCardButton";
 
 export const TaskCards = () => {
 	return (
-		<div>
+		<STaskCardsArea>
 			<TaskCard />
 			<AddTaskCardButton />
-		</div>
+		</STaskCardsArea>
 	);
 };
+
+const STaskCardsArea = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	align-items: flex-start;
+	flex-wrap: wrap;
+`; // flex-boxのflex-wrap: wrap;は自動で折り返しを行ってくれる
--- src/components/task/button/AddTaskCardButton.tsx
+++ src/components/task/button/AddTaskCardButton.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 export const AddTaskCardButton = () => {
-	return <div></div>;
+	return (
+		<SAddTaskCardButtonArea>
+			<SAddTaskCardButton>+</SAddTaskCardButton>
+		</SAddTaskCardButtonArea>
+	);
 };
+
+const SAddTaskCardButtonArea = styled.div`
+	margin-left: 1%;
+	margin-top: 25px;
+`;
+
+const SAddTaskCardButton = styled.button`
+	padding: 10px 18px;
+	border-radius: 9999px;
+	font-size: 30px;
+	background-color: rgb(221, 194, 162);
+	box-shadow: 3px 3px 8px 1px black;
+	cursor: pointer;
+	transition: all 0.3s;
+	&:hover {
+		box-shadow: none;
+		transform: translate(3px, 3px);
+	}
+`;
