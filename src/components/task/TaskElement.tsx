b459fe88c97aa31f8ae2603ac813382ca008907c
sc30gsw <haidoushi875@gmail.com>
2022-05-22 18:56:45 +0900

    タスクの削除機能の実装

--- src/components/task/TaskElement.tsx
+++ src/components/task/TaskElement.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
 import type { Task } from "../../types/task";
 import styled from "styled-components";
+import { TaskListContext } from "../providers/TaskListProvider";
 
 /**
  * 個々のタスクを描画するコンポーネント
@@ -9,10 +10,24 @@ import styled from "styled-components";
  * @returns 個々のタスクを構成する要素
  */
 export const TaskElement: FC<{ task: Task }> = ({ task }) => {
+	// Contextから値を取得
+	const { taskList, setTaskList } = useContext(TaskListContext);
+
+	/**
+	 * タスクを削除する処理
+	 *
+	 * @param id 削除ボタンが押されたタスクのID
+	 */
+	const handleDelete = (id: number) => {
+		// タスクのIDと削除ボタンが押されたタスクのIDが等しくないタスクのみ設定する
+		// IDが一致するもののみ削除する
+		setTaskList(taskList.filter((task) => task.id !== id));
+	};
+
 	return (
 		<STaskBox>
 			<STaskText>{task.text}</STaskText>
-			<STaskButton>
+			<STaskButton onClick={() => handleDelete(task.id)}>
 				<i className="fas fa-trash-alt"></i>
 			</STaskButton>
 		</STaskBox>
@@ -37,4 +52,5 @@ const STaskText = styled.p`
 const STaskButton = styled.button`
 	margin-right: 9px;
 	border: none;
+	cursor: pointer;
 `;
