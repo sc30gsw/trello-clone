f737da710938c06f4339e38cfa137e20f144041f
sc30gsw <haidoushi875@gmail.com>
2022-05-22 12:40:09 +0900

    タスクカード部分のスタイリング

--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -3,14 +3,23 @@ import { TaskAddInput } from "./input/TaskAddInput";
 import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
 import { TaskCardTitle } from "./TaskCardTitle";
 import { Tasks } from "./Tasks";
+import styled from "styled-components";
 
 export const TaskCard = () => {
 	return (
-		<div>
+		<STaskCard>
 			<TaskCardTitle />
 			<TaskCardDeleteButton />
 			<TaskAddInput />
 			<Tasks />
-		</div>
+		</STaskCard>
 	);
 };
+
+const STaskCard = styled.div`
+	width: 250px;
+	padding: 10px 25px;
+	margin: 10px 1%;
+	background-color: rgb(228, 228, 228);
+	border-radius: 5px;
+`;
