faa1c6b73d289fb302bfb9618c0e99ad3e092c7f
sc30gsw <haidoushi875@gmail.com>
2022-05-22 16:38:39 +0900

    個々のタスクをimport

--- src/components/task/Tasks.tsx
+++ src/components/task/Tasks.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from "react";
 import type { Task } from "../../types/task";
+import { TaskElement } from "./TaskElement";
 
-export const Tasks: FC<{ inputText: string; taskList: Task[] }> = ({
-	inputText,
-	taskList,
-}) => {
+export const Tasks: FC<{ taskList: Task[] }> = ({ taskList }) => {
 	return (
 		<div>
-			{taskList.map((list, index) => (
-				<div key={index}>{list.text}</div>
+			{taskList.map((task, index) => (
+				<div key={index}>
+					<TaskElement task={task} />
+				</div>
 			))}
 		</div>
 	);
