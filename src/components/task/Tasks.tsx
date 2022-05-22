067afb708f3b435d130d7a582fde98562ca65bee
sc30gsw <haidoushi875@gmail.com>
2022-05-22 15:55:20 +0900

    タスクカードを表示する処理

--- src/components/task/Tasks.tsx
+++ src/components/task/Tasks.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
+import type { Task } from "../../types/task";
 
-export const Tasks = () => {
-	return <div></div>;
+export const Tasks: FC<{ inputText: string; taskList: Task[] }> = ({
+	inputText,
+	taskList,
+}) => {
+	return (
+		<div>
+			{taskList.map((list, index) => (
+				<div key={index}>{list.text}</div>
+			))}
+		</div>
+	);
 };
