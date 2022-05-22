b2f3018cf9b35aa8e58dca1ea9eab0c3607e0721
sc30gsw <haidoushi875@gmail.com>
2022-05-22 18:56:32 +0900

    グローバルStateの作成

--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useContext } from "react";
 import { TaskAddInput } from "./input/TaskAddInput";
 import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
 import { TaskCardTitle } from "./TaskCardTitle";
 import { Tasks } from "./Tasks";
+import { TaskListContext } from "../providers/TaskListProvider";
 import styled from "styled-components";
-import type { Task } from "../../types/task";
 
 /**
  * タスク一覧を表示するコンポーネント(親コンポーネント)
@@ -14,8 +14,10 @@ import type { Task } from "../../types/task";
 export const TaskCard: FC = () => {
 	// タスク追加入力欄(input要素)を監視するState(初期値: "")
 	const [inputText, setInputText] = useState<string>("");
-	// タスクを配列で保持するState(初期値: 空の配列[])
-	const [taskList, setTaskList] = useState<Task[]>([]);
+
+	// Contextから値を取得
+	const { taskList, setTaskList } = useContext(TaskListContext);
+
 	return (
 		<STaskCard>
 			<TaskCardTitle />
