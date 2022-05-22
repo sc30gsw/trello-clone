b8a69cee2bb130a0275fff96a47c8b1022a102f8
sc30gsw <haidoushi875@gmail.com>
2022-05-22 13:23:52 +0900

    タスク入力コンポーネントの大枠の作成

--- src/components/task/input/TaskAddInput.tsx
+++ src/components/task/input/TaskAddInput.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 export const TaskAddInput = () => {
-	return <div></div>;
+	/**
+	 * Formが送信された時、画面が更新されないようにする処理
+	 *
+	 * @param e Form要素の送信(submit)Event
+	 */
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<STaskAddInput type="text" placeholder="add a task" />
+			</form>
+		</div>
+	);
 };
+
+const STaskAddInput = styled.input`
+	width: 100%;
+	font-size: 1.3rem;
+	padding: 10px 15px;
+	border-radius: 3px;
+	border: none;
+	outline: none;
+`;
