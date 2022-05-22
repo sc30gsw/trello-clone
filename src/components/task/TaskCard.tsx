a9dccdc082ba7f03e7deb1ea889511ae2a14b829
sc30gsw <haidoushi875@gmail.com>
2022-05-22 14:58:02 +0900

    StateをPropsとして渡し、inputTextに値を設定する処理を実装

--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { TaskAddInput } from "./input/TaskAddInput";
 import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
 import { TaskCardTitle } from "./TaskCardTitle";
 import { Tasks } from "./Tasks";
 import styled from "styled-components";
 
-export const TaskCard = () => {
+export const TaskCard: FC = () => {
+	// タスク追加入力欄(input要素)を監視するState(初期値: "")
+	const [inputText, setInputText] = useState<string>("");
+	// タスクを配列で保持するState(初期値: 空の配列[])
+	const [taskList, setTaskList] = useState([]);
 	return (
 		<STaskCard>
 			<TaskCardTitle />
 			<TaskCardDeleteButton />
-			<TaskAddInput />
+			<TaskAddInput
+				inputText={inputText}
+				setInputText={setInputText}
+				taskList={taskList}
+				setTaskList={setTaskList}
+			/>
 			<Tasks />
 		</STaskCard>
 	);
--- src/components/task/input/TaskAddInput.tsx
+++ src/components/task/input/TaskAddInput.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { FC, Dispatch, SetStateAction } from "react";
 import styled from "styled-components";
 
-export const TaskAddInput = () => {
+export const TaskAddInput: FC<{
+	inputText: string;
+	setInputText: Dispatch<SetStateAction<string>>;
+	taskList: never[];
+	setTaskList: Dispatch<SetStateAction<never[]>>;
+}> = ({ inputText, setInputText, taskList, setTaskList }) => {
 	/**
 	 * Formが送信された時、画面が更新されないようにする処理
 	 *
@@ -11,10 +16,23 @@ export const TaskAddInput = () => {
 		e.preventDefault();
 	};
 
+	/**
+	 * Input要素に入力された値をinputTextにセットする処理
+	 *
+	 * @param e Input要素のChangeEvent
+	 */
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputText(e.target.value);
+	};
+
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
-				<STaskAddInput type="text" placeholder="add a task" />
+				<STaskAddInput
+					type="text"
+					placeholder="add a task"
+					onChange={handleChange}
+				/>
 			</form>
 		</div>
 	);
