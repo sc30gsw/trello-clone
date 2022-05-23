ec9b2b117a13c1f663844363938c151510482216
sc30gsw <haidoushi875@gmail.com>
2022-05-23 11:02:59 +0900

    タスクカードの並び替えを行う処理の実装

--- src/components/task/TaskCards.tsx
+++ src/components/task/TaskCards.tsx
@@ -19,8 +19,21 @@ export const TaskCards: FC = () => {
 			draggableId: "item0",
 		},
 	]);
+
+	/**
+	 * ドラッグ&ドロップ時の挙動を扱う処理
+	 *
+	 * @param result ドラッグ&ドロップハンドラーの情報
+	 */
+	const handleDragEnd = (result: any) => {
+		/// タスクカードの並び替えを行う処理の呼び出し
+		reorder(taskCardsList, result.source.index, result.destination.index);
+		// 並び替えたタスクカードをStateに設定する
+		setTaskCardsList(taskCardsList);
+	};
+
 	return (
-		<DragDropContext onDragEnd={() => {}}>
+		<DragDropContext onDragEnd={handleDragEnd}>
 			<Droppable droppableId="droppable" direction="horizontal">
 				{(provided) => (
 					<STaskCardsArea {...provided.droppableProps} ref={provided.innerRef}>
@@ -43,6 +56,24 @@ export const TaskCards: FC = () => {
 			</Droppable>
 		</DragDropContext>
 	);
+};
+
+/**
+ * タスクの並び替えを行う処理
+ *
+ * @param taskCardsList Card配列
+ * @param startIndex 開始位置(タスクカード削除位置)
+ * @param endIndex 終了位置(タスクカード挿入位置)
+ */
+const reorder = (
+	taskCardsList: Card[],
+	startIndex: number,
+	endIndex: number
+) => {
+	// タスクカードを並び替える
+	const removeTaskCard = taskCardsList.splice(startIndex, 1);
+
+	taskCardsList.splice(endIndex, 0, removeTaskCard[0]);
 };
 
 const STaskCardsArea = styled.div`
