be607d6861373182924c0b42b33b05176eb3808b
sc30gsw <haidoushi875@gmail.com>
2022-05-23 10:24:49 +0900

    タスクカードを削除するコンポーネントの作成

--- src/components/task/button/TaskCardDeleteButton.tsx
+++ src/components/task/button/TaskCardDeleteButton.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { Dispatch, FC, SetStateAction, useContext } from "react";
 import styled from "styled-components";
+import { Card } from "../../../types/card";
 
-export const TaskCardDeleteButton = () => {
+/**
+ * タスクカードの削除ボタンを描画し、タスクカードを削除するコンポーネント
+ *
+ * @param taskCardsList タスクカード一覧
+ * @param setTaskCardsList
+ * @param taskCard 個々のタスクカード
+ * @returns タスクカードの削除ボタンを構成する要素
+ */
+export const TaskCardDeleteButton: FC<{
+	taskCardsList: Card[];
+	setTaskCardsList: Dispatch<SetStateAction<Card[]>>;
+	taskCard: Card;
+}> = ({ taskCardsList, setTaskCardsList, taskCard }) => {
+	/**
+	 * タスクカードを削除する処理
+	 *
+	 * @param id 削除ボタンが押されたタスクカードのID
+	 */
+	const taskCardDeleteButton = (id: string) => {
+		// タスクカードを削除する
+		// タスクカードのIDと削除ボタンが押されたタスクカードのIDが等しくないタスクカードのみ設定する
+		// IDが一致するもののみ削除する
+		setTaskCardsList(taskCardsList.filter((taskCard) => taskCard.id !== id));
+	};
 	return (
 		<div>
-			<STaskCardDeleteButton>
+			<STaskCardDeleteButton onClick={() => taskCardDeleteButton(taskCard.id)}>
 				<i className="fas fa-times"></i>
 			</STaskCardDeleteButton>
 		</div>
